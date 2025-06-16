9d011912bcd756f5ccfaf662a55723aa1cbb9192
iidkkk <alvarezdarwiin@gmail.com>
2025-06-16 13:27:28 -0400

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+version = 1
+cloudstream {
+    languages = listOf("es", "en") // Español y Inglés
+    description = "Extensión básica de MultiStream / Basic MultiStream extension"
+    authors = listOf("iidkkk")
+}
